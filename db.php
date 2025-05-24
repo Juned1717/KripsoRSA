9a512d40082ccdc5d4fec61bcb8ba259debfbd51
Juned1717 <junhuijunhuisvt@gmail.com>
2025-05-25 00:58:10 +0700

    db.php

---
+++ db.php
@@ -0,0 +1,16 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "datadokumen";
+
+// Membuat koneksi
+$conn = new mysqli($host, $user, $pass, $db);
+
+// Cek koneksi
+if ($conn->connect_error) {
+  die("Koneksi gagal: " . $conn->connect_error);
+} else {
+  echo "Berhasil connect ke database";
+}
+?>
